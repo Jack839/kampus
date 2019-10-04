5ab90261a499011f976f187d917f4e15e6f2486b
Srikar <nagasrikar.k@gmail.com>
2019-10-04 23:43:51 +0530

    test

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "myHeader.h"
+
+using namespace std;
+
+void printNum(int x) {
+  cout << "int";
+}
+
+void printNum(float x) {
+  cout << "float";
+}
+
+int main() {
+  const int a = 1;
+  float b = 6.328;
+
+  printNum(a);
+  printNum(b);
+}
